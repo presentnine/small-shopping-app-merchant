79506d2862f71d13cd2cdae45e0f7b427f15d7a9
GoonLee <auteur.kor@gmail.com>
2021-11-13 13:02:20 +0900

    Add unapproved store screens

--- app/src/main/java/com/sososhopping/merchant/main/view/ShopListRecyclerViewAdapter.java
+++ app/src/main/java/com/sososhopping/merchant/main/view/ShopListRecyclerViewAdapter.java
@@ -1,6 +1,5 @@
 package com.sososhopping.merchant.main.view;
 
-import android.app.Activity;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,14 +7,10 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentContainerView;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.sososhopping.merchant.MainActivity;
 import com.sososhopping.merchant.R;
 import com.sososhopping.merchant.databinding.ItemShopListBinding;
 import com.sososhopping.merchant.main.model.ShopListModel;
@@ -54,18 +49,36 @@ public class ShopListRecyclerViewAdapter extends RecyclerView.Adapter<ShopListRe
                 Glide.with(holder.itemView.getContext())
                         .load(R.drawable.ic_baseline_hourglass_empty_24)
                         .into(holder.mVerified);
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Navigation.findNavController((View) (v.getParent().getParent().getParent().getParent().getParent().getParent())).navigate(R.id.action_mainFragment_to_storeRegisterPendingFragment);
+                    }
+                });
                 break;
             }
             case "REJECT": {
                 Glide.with(holder.itemView.getContext())
                         .load(R.drawable.ic_baseline_remove_circle_outline_24)
                         .into(holder.mVerified);
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Navigation.findNavController((View) (v.getParent().getParent().getParent().getParent().getParent().getParent())).navigate(R.id.action_mainFragment_to_storeRegisterRejectedFragment);
+                    }
+                });
                 break;
             }
             case "SUSPEND": {
                 Glide.with(holder.itemView.getContext())
                         .load(R.drawable.ic_baseline_not_interested_24)
                         .into(holder.mVerified);
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Navigation.findNavController((View) (v.getParent().getParent().getParent().getParent().getParent().getParent())).navigate(R.id.action_mainFragment_to_storeRegisterDeniedFragment);
+                    }
+                });
                 break;
             }
         }
